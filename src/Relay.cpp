be9bca502e4900ca4e137d54921d7be930e34d51
poljant <poljant@post.pl>
2019-10-31 17:55:40 +0100

    Add function inverted

--- src/Relay.cpp
+++ src/Relay.cpp
@@ -4,40 +4,64 @@
  *  Copyright (c) 2016. All rights reserved.
  *
  *      Author: Jan Trzciński  <poljant@post.pl>
+ * modyfied 28-10-2019
  */
 
 #include <Arduino.h>
 #include "Relay.h"
 
 Relay::Relay() {
-	pin = 0;
+    pin = 0;
+    inv = false;
 }
 
 Relay::~Relay()
 {
 
 }
+void Relay::begin(void)
+{
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+}
 void Relay::begin(int p)
 {
-	pin = p;
-	pinMode(pin, OUTPUT);
-	digitalWrite(pin, LOW);
+    pin = p;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
 }
 
+void Relay::begin(int p ,bool r)
+{
+    pin = p;
+    inv = r;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+}
 void Relay::setOn(void)
 {
-	digitalWrite(pin, HIGH);
+    digitalWrite(pin, inv ? LOW : HIGH);
 }
 void Relay::setOff(void)
 {
-	digitalWrite(pin, LOW);
+    digitalWrite(pin, inv ? HIGH : LOW);
 }
 int Relay::read(void)
 {
-	return digitalRead(pin);
+    return (digitalRead(pin) and !inv) ? HIGH : LOW ;
 }
-//void Relay::setPin(int p){
-//	pin = p;
-//}
-
-
+bool Relay::isOn(void)
+{
+    return (digitalRead(pin) and !inv);
+}
+void Relay::setPin(int p){
+    pin = p;
+}
+bool Relay::inverted(bool r)
+{
+    inv= r;
+    return inv;
+}
+bool Relay::inverted(void){
+    return inv;
+}
--- src/Relay.h
+++ src/Relay.h
@@ -7,21 +7,27 @@
  *
  *      Klasa Relay umożliwia obsługę i kontrolę pracy przkaźnika
  *      podłączonego do określonego pinu.
-
- */
+ *   modyfikowano 28-10-2019
+  */
 
 #ifndef RELAY_H_
 #define RELAY_H_
 
 class Relay {
-	int pin;
+    int pin;
+    bool inv;
 public:
-	Relay();
-	virtual ~Relay();
-//	void setPin(int);
-	void begin(int);
-	void setOn();
-	void setOff();
-	int read();
+    Relay();
+    virtual ~Relay();
+    void setPin(int);
+    void begin();
+    void begin(int );
+    void begin(int , bool );
+    void setOn();
+    void setOff();
+    int read();
+    bool isOn();
+    bool inverted();
+    bool inverted(bool);
 };
 #endif /* RELAY_H_ */
